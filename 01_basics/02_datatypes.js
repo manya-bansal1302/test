67dc3508d420a1f1f3bcebf211cd47c10e180410
Manya Bansal <manya1262.becse24@chitkara.edu.in>
2024-12-23 16:49:07 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+//alert (3 + 3) we arre usinh node js not browser
+
+console.log(3 + 3)
+
+console.log("manya")
+
+let name = "manya"//string datatype
+let agr = 18 //number datatype
+let isLoggedIn = false // boolean datatype
+
+//primitive datatypes
+//number = 2 to power 53
+//bigint
+//string = ""
+//boolean = true/false
+//null = stand alone value
+//undefined 
+//symbol
+
+//object
+
+console.log(typeof "manya");
+console.log(typeof "null");
+console.log(typeof null);
+console.log(typeof undefined);
