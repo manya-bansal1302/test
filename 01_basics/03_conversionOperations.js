8fab8723ea151ef1aa529b51e3243d21b37c618b
Manya Bansal <manya1262.becse24@chitkara.edu.in>
2024-12-23 17:10:13 +0530

    conversion operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,14 @@
+let score = "33"
+console.log(typeof score);
+
+let valueInNumber = Number(score) //change the datatype
+console.log(typeof valueInNumber);
+
+//"22" = 33
+// "33abc" = NAN (not a number)
+//true = 1 ; false = 0
+let isLoggedIn = 1
+let booleanIsLogedIn = Boolean(isLoggedIn)
+console.log(booleanIsLogedIn);
+//"" = false
+// "hitesh" = true
