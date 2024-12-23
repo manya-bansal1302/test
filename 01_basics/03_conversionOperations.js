7b43809198874b68dcd22cf4c4e972d74217b637
Manya Bansal <manya1262.becse24@chitkara.edu.in>
2024-12-23 17:31:30 +0530

    conversion operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,14 +1,41 @@
 let score = "33"
-console.log(typeof score);
+//console.log(typeof score);
 
 let valueInNumber = Number(score) //change the datatype
-console.log(typeof valueInNumber);
+//console.log(typeof valueInNumber);
 
 //"22" = 33
 // "33abc" = NAN (not a number)
 //true = 1 ; false = 0
 let isLoggedIn = 1
 let booleanIsLogedIn = Boolean(isLoggedIn)
-console.log(booleanIsLogedIn);
+//console.log(booleanIsLogedIn);
 //"" = false
 // "hitesh" = true
+
+/************* operations **********/
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Manya"
+let str3 = str1 + str2
+//console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+let gameCounter = 100
+gameCounter ++;
+console.log(gameCounter);
