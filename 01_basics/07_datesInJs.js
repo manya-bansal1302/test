695ab8ca53697aa063f0228f89b0231ef16ef351
Manya Bansal <manya1262.becse24@chitkara.edu.in>
2024-12-24 12:11:50 +0530

    date and time

---
+++ 01_basics/07_datesInJs.js
@@ -0,0 +1,15 @@
+let myDate =  new Date()
+//console.log(myDate);
+//console.log(myDate.toString());
+//console.log(myDate.toDateString());
+//console.log(myDate.toISOString());
+//console.log(myDate.toJson);
+//console.log(typeof myDate);
+//console.log(myDate.toLocaleString)
+
+let myCreatedDate = new Date(2024,0,24)
+//console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
