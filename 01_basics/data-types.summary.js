584e56d87a5d55b2b4dd8dea64610ecbc7298fb8
Manya Bansal <manya1262.becse24@chitkara.edu.in>
2024-12-24 07:35:05 +0530

    datatypes summary

---
+++ 01_basics/data-types.summary.js
@@ -0,0 +1,18 @@
+//primitive datatypes (7) call by value
+// 1.String 2.Number 3.Boolean 4.Null 5.Undefined 6.Symbol 7. BigInt
+
+const id = Symbol('123');
+const anotherId = Symbol('123')
+console.log( id == anotherId) //false
+
+//non-primitive reference 
+//Array, Objects,Functions
+
+const Heros = ["shaktiman", "naagraj", "doga"] //Arrays
+ let myObj = {  //Objects
+  name: "manya",
+  age: 22,
+}
+const myFunction = function(){ //functions
+  console.log("Hello World");
+}
