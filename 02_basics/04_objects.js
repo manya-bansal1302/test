d7f83f61ef3e321aea4780e0fa588aca3e330e25
Manya Bansal <manya1262.becse24@chitkara.edu.in>
2024-12-24 15:09:14 +0530

    object-3

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,50 @@
+//objects constructor (singletone)
+//const tinderUser = new Object()  //singl tone object 
+const tinderUser ={}  //output will be empty  non-single tone object
+
+
+tinderUser.id = "123ac"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false 
+//console.log(tinderUser) // output will be {}
+
+const regularUser = {
+  email:"ome@gmail.com",
+  fullname :{
+      userfullname:{
+        firstname:"Manya",
+        lastname:"bansal"
+      }
+  }
+}
+//console.log(regularUser)
+//console.log(regularUser.fullname.userfullname.firstname)
+
+const object1 = { 1:"a" , 2:"b"}
+const object2 = {3:"c" , 4:"d"}
+const object4 = {5:"e" , 6:"f"}
+
+//join two object
+//const obj3 = Object.assign(object1,object2) (1st way)
+//const obj3 = Object.assign({},object1,object2,object4) //{} => represents target (2nd way)
+
+const obj3 = {...object1,...object2,...object4}  //(3rd way and easy way in array also thi is used to add two aur more array)
+//console.log(obj3);
+
+//database se aaya hai 
+const User = [
+  {
+    id:1,
+    email:"h@gmail.com"
+  },
+  {
+    id:2,
+    email:"g@gmail.com"
+  }
+]
+//console.log(User[1].email)
+//console.log(User[0].id)
+
+//console.log(tinderUser)
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
